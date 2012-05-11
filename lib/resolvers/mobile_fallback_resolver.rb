b01dd75095f441e06c8e026fab640287da3642bb
Jack Dempsey <jack.dempsey@gmail.com>
2012-05-10 17:27:19 -0700

    correctly namespace inside module name

--- lib/resolvers/mobile_fallback_resolver.rb
+++ lib/resolvers/mobile_fallback_resolver.rb
@@ -1,10 +1,12 @@
-class MobileFallbackResolver < ::ActionView::FileSystemResolver
-  def find_templates(name, prefix, partial, details)
-    if details[:formats] == [:mobile]
-      # Add a fallback for html, for the case where, eg, 'index.html.haml' exists, but not 'index.mobile.haml'
-      details = details.dup
-      details[:formats] = [:mobile, :html]
+module Resolvers
+  class MobileFallbackResolver < ::ActionView::FileSystemResolver
+    def find_templates(name, prefix, partial, details)
+      if details[:formats] == [:mobile]
+        # Add a fallback for html, for the case where, eg, 'index.html.haml' exists, but not 'index.mobile.haml'
+        details = details.dup
+        details[:formats] = [:mobile, :html]
+      end
+      super
     end
-    super
   end
 end
