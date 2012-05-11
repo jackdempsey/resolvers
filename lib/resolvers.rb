1c3f52d5b6d2c558867c81db2623f680da96fe7b
Jack Dempsey <jack.dempsey@gmail.com>
2012-05-10 18:10:41 -0700

    both seem to work. trying to see which is better if any

--- lib/resolvers.rb
+++ lib/resolvers.rb
@@ -1,3 +1,15 @@
 module Resolvers
   autoload :MobileFallbackResolver, 'resolvers/mobile_fallback_resolver'
+
+  ActiveSupport.on_load(:action_controller) do
+    append_view_path(MobileFallbackResolver.new('app/views'))
+  end
+
+  # class Railtie < ::Rails::Railtie
+  #   initializer 'resolvers.mobile_fallback_resolver' do |app|
+  #     ActiveSupport.on_load(:action_controller) do
+  #       append_view_path(MobileFallbackResolver.new('app/views'))
+  #     end
+  #   end
+  # end
 end
