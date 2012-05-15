71c40152c1dc11c25eea119698c7fc64141ef30b
Jack Dempsey <jack.dempsey@gmail.com>
2012-05-15 16:34:59 -0700

    per jose valim's comment, just do simple on_load, railtie not needed

--- lib/resolvers.rb
+++ lib/resolvers.rb
@@ -4,12 +4,4 @@ module Resolvers
   ActiveSupport.on_load(:action_controller) do
     append_view_path(MobileFallbackResolver.new('app/views'))
   end
-
-  # class Railtie < ::Rails::Railtie
-  #   initializer 'resolvers.mobile_fallback_resolver' do |app|
-  #     ActiveSupport.on_load(:action_controller) do
-  #       append_view_path(MobileFallbackResolver.new('app/views'))
-  #     end
-  #   end
-  # end
 end
